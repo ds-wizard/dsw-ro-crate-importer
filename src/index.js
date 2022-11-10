e57e746fce01d0859a17f180418c8b11433f7f0a
Jan Slifka <jan.slifka@gmail.com>
2022-11-10 18:24:48 +0100

    Make it work with DSW

--- src/index.js
+++ src/index.js
@@ -1,39 +1,34 @@
-window.opener = {
-    postMessage: () => {
-        window.postMessage({type: 'ready'}, '*')
-    }
-}
-
 import DSWImporter from '@ds-wizard/importer-sdk'
-import {RoCrateProcessor} from './processor'
-import exampleData from './example/data'
-import {showError} from './ui'
+import { RoCrateProcessor } from './processor'
+import { showError } from './ui'
 
 window.addEventListener('load', (event) => {
     console.log("Initialing the importer...")
+    
     const importer = new DSWImporter()
     importer
         .init()
         .then(() => {
-            console.log("Importer initialized!")
+            document.getElementById('main').classList.remove('hidden')
+
             const roCrateProcessor = new RoCrateProcessor(importer)
-            roCrateProcessor.run(exampleData) // TODO remove the line
             const fileSelector = document.getElementById('file-input')
-            fileSelector.addEventListener('change', (event) => {
+
+            fileSelector.addEventListener('change',  (event) => {
                 const fileList = event.target.files
-                console.log(fileList)
                 if (fileList.length !== 1) {
                     alert('File not selected...')
                     return
                 }
-                const file = fileList[0]
 
+                const file = fileList[0]
                 const reader = new FileReader()
                 reader.addEventListener('load', (event) => {
                     let data = null
                     try {
                         data = JSON.parse(event.target.result)
                     } catch (error) {
+                        console.error(error)
                         showError('Failed to parse JSON file.')
                         return
                     }
@@ -41,9 +36,9 @@ window.addEventListener('load', (event) => {
                         return roCrateProcessor.run(data)
                             .then(() => {
                                 try {
-                                    logResult(importer)
                                     importer.send()
                                 } catch (error) {
+                                    console.error(error)
                                     showError('Failed to send data back to the Wizard.')
                                 }
                             })
@@ -62,7 +57,5 @@ window.addEventListener('load', (event) => {
         })
         .catch(error => {
             console.error(error)
-            throw error
         })
-
 })
